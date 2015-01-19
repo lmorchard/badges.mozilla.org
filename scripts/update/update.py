d5ac61dcbe98130c2612edb245aa6d8895589ea5
Les Orchard <me@lmorchard.com>
2015-01-19 10:57:57 -0500

    #96: Try deleting virtualenv before updating dependencies

--- scripts/update/update.py
+++ scripts/update/update.py
@@ -53,6 +53,7 @@ def setup_dependencies(ctx):
         # writing, but because virtualenv is using it, it fails.
         # So we delete it and let virtualenv create a new one.
         ctx.local('rm -f virtualenv/bin/python')
+        ctx.local('rm -rf virtualenv')
         ctx.local('virtualenv --no-site-packages virtualenv')
 
         # Activate virtualenv to append to path.
